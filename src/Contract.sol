8e80341529a08f702daa08a8cb5ec1d6a3dea269
AstronautSleuth <astronautsleuth@gmail.com>
2022-06-20 20:55:15 +0800

    first commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,43 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-contract Contract { }
+contract A {
+    uint256 data;
+    constructor(uint256 _data) {
+        data = _data;
+    }
+    function getData(uint256 _divisor) public view returns(uint256) {
+        if(_divisor == 0) {
+            revert();
+        } else if (_divisor == 1) {
+            assert(false);
+        } else if (_divisor >= 10) {
+            // require(_divisor < 10, "divisor too yuge!"); // Require(x, msg) is equivalent to if(x) revert(msg);
+            revert("divisor too yuge!");
+        }
+        return data / _divisor;
+    }
+}
+
+contract B {
+    uint256 data;
+    constructor(uint256 _data) {
+        data = _data;
+    }
+    function getData() public view returns(uint256) {
+        return data;
+    }
+}
+
+contract C {
+    function tryGetData(address _a, uint256 _divisor) public view returns (uint256, string memory) {
+        try A(_a).getData(_divisor) returns (uint256 v) {
+            return(v, "nil");
+        } catch Error(string memory s) {
+            return(420, s);
+        } catch Panic(uint c) {
+            return(c, "panic!");
+        } catch (bytes memory) {
+            return(1337, "catch all!");
+        }
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,22 +2,51 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-
-import "src/Contract.sol";
+import { A, B, C } from "src/Contract.sol";
 
 contract TestContract is Test {
-    Contract c;
+    A a;
+    B b;
+    C c;
 
     function setUp() public {
-        c = new Contract();
+        a = new A(10);
+        b = new B(50);
+        c = new C();
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testSuccess() public {
+        // Call tryGetData with a _divisor > 1 and < 10
+        // expected output is (v, "nil")
+        (uint v, string memory s) = c.tryGetData(address(a), 5);
+        assertEq(v, 2);
+        assertEq(s, "nil");
     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testCatchError() public {
+        // Call tryGetData with a _divisor == 10
+        // expected output is (420, "divisor too yuge!")
+        (uint v, string memory s) = c.tryGetData(address(a), 10);
+        assertEq(v, 420);
+        assertEq(s, "divisor too yuge!");
+
+    }
+
+    function testCatchPanic() public {
+        // Call tryGetData with a _divisor == 1
+        // expected output is (c, "panic!")
+        // where c is one of https://docs.soliditylang.org/en/v0.8.13/control-structures.html?highlight=Panic#panic-via-assert-and-error-via-require
+        (uint v, string memory s) = c.tryGetData(address(a), 1);
+        assertEq(v, 1);
+        assertEq(s, "panic!");
+
+    }
+
+    function testCatchAll() public {
+        // Call tryGetData with any value for _divisor
+        // expected output is (v, "nil")
+        (uint v, string memory s) = c.tryGetData(address(b), 5);
+        assertEq(v, 1337);
+        assertEq(s, "catch all!");
     }
 }
